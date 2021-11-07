1cc9d661b1cd2781c56327c72ca817afaf7d7c42
jesud <jesud@10.0.0.30>
2021-11-07 13:04:31 -0800

    Updated Menu:
    Settings menu class added. Both manu and settins menu are complete,
    except passing cerain values. Along with a quit button
    
    Next to be done is Collections Menu.

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -12,6 +12,7 @@ public class MainApplication extends GraphicsProgram {
 	private SomePane somePane;
 	private MenuPane menu;
 	private Collection collection;
+	private SettingsMenu settings;
 	
 	/* Method: setupInteractions
 	 * -------------------------
@@ -102,6 +103,7 @@ public class MainApplication extends GraphicsProgram {
 	public void run() {
 		//System.out.println("Let's make something awesome!");
 		somePane = new SomePane(this);
+		settings = new SettingsMenu(this);
 		menu = new MenuPane(this);
 		setupInteractions();
 		switchToMenu();
@@ -112,14 +114,18 @@ public class MainApplication extends GraphicsProgram {
 	}
 
 	public void switchToSome() {
+		//switchToScreen(somePane);
 		if (menu.getOption() == 1) {
 			//switchToScreen();
 		}
-		if (menu.getOption() == 2) {
+		else if (menu.getOption() == 2) {
 			switchToScreen(collection);
 		}
-		if (menu.getOption() == 3) {
-			//switchToScreen();
+		else if (menu.getOption() == 3) {
+			switchToScreen(settings);
+		}
+		else if (menu.getOption() == 4) {
+			System.exit(4);
 		}
 	}
 
--- src/MenuPane.java
+++ src/MenuPane.java
@@ -1,7 +1,5 @@
 import java.awt.Color;
-
 import java.awt.event.MouseEvent;
-
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
@@ -20,49 +18,65 @@ public class MenuPane extends GraphicsPane {
 	private GImage COLLECTIONS;
 	private GButton SETTINGS_BUTTON;
 	private GImage SETTINGS;
+	private GButton QUIT_BUTTON;
+	private GImage QUIT;
 	private final int BUTTON_SIZE = 50;
-	public int OPTION;
+	private int OPTION;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		rect = new GButton("Next", app.getWidth()/2-BUTTON_SIZE/2, app.getHeight()/2-BUTTON_SIZE/2, BUTTON_SIZE, BUTTON_SIZE);
-		rect.setFillColor(Color.RED);
+//		rect = new GButton("Next", app.getWidth()/2-BUTTON_SIZE/2, app.getHeight()/2-BUTTON_SIZE/2, BUTTON_SIZE, BUTTON_SIZE);
+//		rect.setFillColor(Color.RED);
 		// Background
 		MAIN_MENU = new GImage("media/MenuScreen/zooHDMENU.jpeg", -20, 0);
+		MAIN_MENU.sendToBack();
 		//Title
 		TITLE  = new GImage("media/MenuScreen/Title.png",180,175);
 		//Play with button.
 		PLAY = new GImage("media/MenuScreen/playButton.png",605, 450);
-		PLAY_BUTTON = new GButton("",605,450,150,65);
+		PLAY_BUTTON = new GButton(" ",605,450,150,65);
+		PLAY_BUTTON.setFillColor(new Color(255,225,53));
+		PLAY_BUTTON.setVisible(false);
 		// COllections
-		COLLECTIONS_BUTTON = new GButton("", 500,555, 360,65);
+		COLLECTIONS_BUTTON = new GButton(" ", 500,555, 360,65);
+		COLLECTIONS_BUTTON.setFillColor(new Color(255,225,53));
+		COLLECTIONS_BUTTON.setVisible(false);
 		COLLECTIONS = new GImage("media/MenuScreen/collections.png", 500,550);
 		// Settings  with button.
-		SETTINGS_BUTTON = new GButton("", 560,650, 260,65);
+		SETTINGS_BUTTON = new GButton(" ", 560,650, 260,65);
+		SETTINGS_BUTTON.setFillColor(new Color(255,225,53));
+		SETTINGS_BUTTON.setVisible(false);
 		SETTINGS = new GImage("media/MenuScreen/settings.png", 560, 650);
-	
+		// OPT OUT
+		QUIT_BUTTON = new GButton(" ", 560, 750, 260,65);
+		QUIT_BUTTON.setVisible(false);
+		QUIT = new GImage("media/MenuScreen/QUIT.png",625, 750);
+		
+		
 		
 	}
 
 	@Override
 	public void showContents() {
-		//program.add(rect);
+//		program.add(rect);	
 		program.add(MAIN_MENU);
 		program.add(TITLE);
-		program.add(PLAY_BUTTON);
 		program.add(PLAY);
-		program.add(COLLECTIONS_BUTTON);
+		program.add(PLAY_BUTTON);
 		program.add(COLLECTIONS);
-		program.add(SETTINGS_BUTTON);
+		program.add(COLLECTIONS_BUTTON);
 		program.add(SETTINGS);
+		program.add(SETTINGS_BUTTON);
+		program.add(QUIT);
+		program.add(QUIT_BUTTON);
 		
 		
 	}
 
 	@Override
 	public void hideContents() {
-		//program.remove(rect);
+//		program.remove(rect);
 		program.remove(MAIN_MENU);
 		program.remove(TITLE);
 		program.remove(PLAY_BUTTON);
@@ -75,14 +89,27 @@ public class MenuPane extends GraphicsPane {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-//		GObject obj = program.getElementAt(e.getX(), e.getY());
+		GObject obj = program.getElementAt(e.getX(), e.getY());
 //		if (obj == rect) {
 //			program.switchToSome();
 //		}
-		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == PLAY_BUTTON) {
-			
+		if (obj == PLAY_BUTTON && obj instanceof GButton) {
 			program.switchToSome();
+			setOption(1);
+		}
+		if (obj == COLLECTIONS_BUTTON && obj instanceof GButton) {
+			program.switchToSome();
+			setOption(2);
+		}
+		if (obj == SETTINGS_BUTTON && obj instanceof GButton) {
+			setOption(3);
+			program.switchToSome();
+			
+		}
+		if (obj == QUIT_BUTTON && obj instanceof GButton) {
+			setOption(4);
+			program.switchToSome();
+			
 		}
 		
 	}
