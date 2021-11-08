6e854d9a946b7a1b73fbd766a8b02749179b699c
TahpRamen <86633301+RamonAra209@users.noreply.github.com>
2021-11-08 10:32:20 -0800

    Made updateLocation in Location.java more simple

--- src/Location.java
+++ src/Location.java
@@ -5,43 +5,35 @@ public class Location {
 	
 	
 	
-public Location (int xLocation, int yLocation) {
-	this.xAxis = xLocation;
-	this.yAxis = yLocation;
-	
-	
-}
+	public Location (int xLocation, int yLocation) {
+		this.xAxis = xLocation;
+		this.yAxis = yLocation;
+	}
 
-public int getXAxis() {
-	return xAxis;
-}
+	public int getXAxis() {
+		return xAxis;
+	}
 
-public int getYAxis() {
-	return yAxis;
-}
+	public int getYAxis() {
+		return yAxis;
+	}
 
-public void setXAxis(int xAxis) {
-	this.xAxis = xAxis;
-}
-
-public void setYAxis(int yAxis) {
-	this.yAxis = yAxis;
-}
+	public void updateLocation(int xAxis, int yAxis) {
+		this.xAxis = xAxis;
+		this.yAxis = yAxis;
+	}
 
 
+	public static void main (String [] args) {
+		Location one = new Location(3, 4);
+		Location two = new Location (1,6);
+		
+		System.out.println("First Test: X-axis:" + one.getXAxis() + ",Y-Axis:" + one.getYAxis());
+		
+		System.out.println("Second Test: X-axis:" + two.getXAxis() + ",Y-Axis:" + two.getYAxis());
 
-public static void main (String [] args) {
-	Location one = new Location(3, 4);
-	Location two = new Location (1,6);
-	two.setXAxis(two.getXAxis()+1);
-	two.setYAxis(two.getYAxis()-1);
-	
-	System.out.println("First Test: X-axis:" + one.getXAxis() + ",Y-Axis:" + one.getYAxis());
-	
-	System.out.println("Second Test: X-axis:" + two.getXAxis() + ",Y-Axis:" + two.getYAxis());
-
-	 
-}
+		
+	}
 
 
 
