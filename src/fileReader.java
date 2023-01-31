8710bbfa9b03511663df39d6f3dd3a813de4d924
byrat <b_tran38@u.pacific.edu>
2023-01-31 00:03:33 -0800

    Adding in reading in of the x coordinate of the enemy

--- src/fileReader.java
+++ src/fileReader.java
@@ -1,6 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+
+
 public class fileReader {
 	public static String getJsonInfo(String filename) {
 		String jsontxt = "";
@@ -25,7 +31,21 @@ public class fileReader {
 
 public static void main(String[] args) {
 		String strJson = getJsonInfo("C:\\Users\\Bryant Tran\\git\\groupproject-avengers\\src\\data.json");
-		System.out.println(strJson);
+		//System.out.println(strJson);
+		
+		try {
+			JSONParser parser = new JSONParser();
+			Object object = parser.parse(strJson);
+			JSONObject mainObject = (JSONObject) object;
+			
+			//Enemy// 
+			String enemyXpos = (String) mainObject.get("enemyXpos");
+			System.out.println(enemyXpos);
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 	
 }
